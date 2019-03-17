a59ed0bb3193605f499e1d7e6c32b8c0e3272f5d
Chirag Ramani <chirag.ramani7@gmail.com>
2019-03-17 17:16:46 +0530

    Added source

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-
+// Source: https://stackoverflow.com/questions/43520498/react-router-private-routes-redirect-not-working - 6
 const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => (
   <Route
     {...rest}
     render={props =>
-      isAuthenticated === true ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to="/" />
-      )
+      isAuthenticated === true ? <Component {...props} /> : <Redirect to="/" />
     }
   />
 );
