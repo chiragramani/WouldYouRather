4cc0ed086743b95a1f10d4c3b2059b2219b5095a
Chirag Ramani <chirag.ramani7@gmail.com>
2019-03-17 22:27:15 +0530

    Added answered and unanswered selection tabs

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,45 @@
 import React, { Component } from "react";
 
 export default class Dashboard extends Component {
+  state = {
+    filter: "unanswered-questions"
+  };
+  handleFilterChange = e => {
+    e.preventDefault();
+    this.setState({
+      filter: e.target.name
+    });
+  };
+
   render() {
-    return <div>Dashboard</div>;
+    const { filter } = this.state;
+    return (
+      <div>
+        <div className="filters">
+          <button
+            className={
+              filter === "unanswered-questions"
+                ? "filterButton button--on"
+                : "filterButton button--off"
+            }
+            name="unanswered-questions"
+            onClick={this.handleFilterChange}
+          >
+            Unanswered Questions
+          </button>
+          <button
+            className={
+              filter === "answered-questions"
+                ? "filterButton button--on"
+                : "filterButton button--off"
+            }
+            name="answered-questions"
+            onClick={this.handleFilterChange}
+          >
+            Answered Questions
+          </button>
+        </div>
+      </div>
+    );
   }
 }
